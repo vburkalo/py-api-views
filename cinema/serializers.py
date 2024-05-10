9420baa108ddab814e4ed8e1713dc3d234c74310
vital <vitaliyburkalo81@gmail.com>
2024-05-10 18:26:12 +0300

    created and updated serializers for existing models

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,16 +1,45 @@
 from rest_framework import serializers
+from cinema.models import Actor, Genre, CinemaHall, Movie
 
-from cinema.models import Movie
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name"]
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row"]
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Actor.objects.all()
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Genre.objects.all()
+    )
+
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "description", "actors", "genres", "duration"]
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors_data = validated_data.pop("actors", [])
+        genres_data = validated_data.pop("genres", [])
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors_data)
+        movie.genres.set(genres_data)
+        movie.save()
+        return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
@@ -19,6 +48,13 @@ class MovieSerializer(serializers.Serializer):
         )
         instance.duration = validated_data.get("duration", instance.duration)
 
-        instance.save()
+        actors_data = validated_data.get("actors")
+        if actors_data is not None:
+            instance.actors.set(actors_data)
 
+        genres_data = validated_data.get("genres")
+        if genres_data is not None:
+            instance.genres.set(genres_data)
+
+        instance.save()
         return instance
